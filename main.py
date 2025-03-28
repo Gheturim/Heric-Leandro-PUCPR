9148a82ca99a9a4155d05e9fdbab7a9a55c6c435
Heric Leandro <hericllh@gmail.com>
2025-03-27 21:11:25 -0300

    voltando pós formatação

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 import random
 app = FastAPI()
 
-@app.get("/")
+@app.get("/Hello World")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/testando")
 @app.get("/teste1")
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(1, 1000) }
+    return {"teste": True, "num_aleatorio": random.randint(1, 2000) }
