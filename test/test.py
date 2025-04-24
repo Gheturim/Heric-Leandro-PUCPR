64130016596cca28ef7b49c25e3daa9bb7a4e307
Heric Leandro <hericllh@gmail.com>
2025-04-23 21:28:18 -0300

    teste2

---
+++ test/test.py
@@ -0,0 +1,30 @@
+
+from src.main import *
+from unittest.mock import patch
+
+
+def teste_root():
+    assert root() == {"message": "Hello World"}
+
+def test_funcaoteste():
+    with patch('radom.radint', return_value=12345):
+        result = funcaoteste()
+
+    assert result == {"teste": True, "num_aleatorio": 12345 }
+
+def test_create_estudante():
+    estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=False)
+    assert  estudante_teste == create_estudante(estudante_teste)
+
+
+def test_update_estudante_positivo():
+    assert not update_estudante(-5)
+
+def test_update_estudante_negativo():
+    assert update_estudante(10)
+
+def teste_delete_estudante_negativo():
+    assert not delete_estudante(-5)
+
+def teste_delete_estudante_positivo():
+    assert delete_estudante(10)
