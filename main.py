0c945ff726a0fd6b716a7a41466afef413fd2ac3
Heric Leandro <hericllh@gmail.com>
2025-04-23 21:46:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from fastapi import FastAPI
 import random
+from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+
+class Estudante(BaseModel):
+    name: str
+    curso: str
+    ativo: bool
 
 @app.get("/Hello World")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/testando")
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(1, 5000) }
+    return {"teste": True, "num_aleatorio": random.randint(0, 57000)}
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+@app.put("/estudantes/update/{id_estudante}")
+async def update_estudante(id_estudante: int,):
+    return id_estudante > 0
+
+
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
